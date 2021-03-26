3f2f4a78a814476d6ce65bede143b155c6adea95
Orrin19 <67312910+Orrin19@users.noreply.github.com>
2021-03-26 14:04:51 +0700

    1.6.5.5 new version

--- bot.js
+++ bot.js
@@ -361,6 +361,14 @@ bot.on('message', async (message) => {
   }
 });
 
+// !rickroll meme
+bot.on('message', async (message) => {
+  if (message.author.bot) return;
+  if (message.content.startsWith(prefix + 'rickroll')) {
+    message.channel.send('https://media.discordapp.net/attachments/664491015914258460/824896135902134283/Rickrolling.gif');
+  }
+});
+
 // !clean
 bot.on('message', async (message) => {
   if (message.author.bot) return;
@@ -875,10 +883,13 @@ bot.on('message', async (message) => {
   }
 });
 
-// Выдача ролей на Ламоране
+// Выдача ролей и автоприветствие на Ламоране
 bot.on('guildMemberAdd', async (member) => {
   let role = member.guild.roles.cache.find((r) => r.name === 'Начинающий');
   await member.roles.add(role.id);
+  member.send(
+    'Здарова, уёбки\nДобро пожаловать в Ад нахуй\nЗдесь блядь рп\nРП БЛЯТЬ\nЕСЛИ НЕ УМЕЕШЬ РПШИТЬ\nИди на хуй отсюда, пожалуйста\nИ хватит задавать свои тупые вопросы\nИди в канал общение\nВыдержи горц мата\nХентая\nПорнухи\nИ спроси'
+  );
 });
 
 // Ссылки на вики на Ламоране
