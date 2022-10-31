5c10fd53421a3d890cf3a502002b2134c241b4b6
Orrin19 <daniil.orrin@yandex.ru>
2022-10-31 09:34:58 +0700

    autoban

--- src/listeners/guildMemberAdd.ts
+++ src/listeners/guildMemberAdd.ts
@@ -3,6 +3,9 @@ import Discord from 'discord.js';
 export default (client: Discord.Client): void => {
   client.on('guildMemberAdd', async (member: Discord.GuildMember) => {
     if (member.guild.id !== '664491015914258452') return;
+    if (member.user.username.includes('自動の')) {
+      await member.ban({ reason: 'Спам' });
+    }
     const noviceRole = member.guild.roles.cache.find(
       (r) => r.name === 'Начинающий'
     ) as Discord.RoleResolvable;
