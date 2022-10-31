c7597c5e38b6512eef4978f93b28b8d7598287c8
Orrin19 <daniil.orrin@yandex.ru>
2022-10-31 14:05:34 +0700

    bugfix

--- src/listeners/guildMemberAdd.ts
+++ src/listeners/guildMemberAdd.ts
@@ -5,6 +5,7 @@ export default (client: Discord.Client): void => {
     if (member.guild.id !== '664491015914258452') return;
     if (member.user.username.includes('自動の')) {
       await member.ban({ reason: 'Спам' });
+      return;
     }
     const noviceRole = member.guild.roles.cache.find(
       (r) => r.name === 'Начинающий'
