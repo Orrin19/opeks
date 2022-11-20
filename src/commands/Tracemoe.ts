54e0f5c2f1fcce4c84d8cef579cc68d48162db99
Orrin19 <daniil.orrin@yandex.ru>
2022-11-20 12:22:37 +0700

    trace.moe update

--- src/commands/Tracemoe.ts
+++ src/commands/Tracemoe.ts
@@ -1,8 +1,9 @@
-import Discord from 'discord.js';
+import Discord, { DiscordjsTypeError } from 'discord.js';
 import { TraceMoe } from 'trace.moe.ts';
 import { Command } from '../Command';
 import { Footer } from '../custom/Footer';
 import config from '../config';
+import { getTime } from '../custom/commonFunctions';
 
 export const Tracemoe: Command = {
   name: 'trace-moe',
@@ -34,31 +35,59 @@ export const Tracemoe: Command = {
   ) => {
     const api = new TraceMoe();
     const link = interaction.options.get('link', true)?.value as string;
-    const request = await api.fetchAnime(link);
+    const request = await api.fetchAnime(link, { anilistInfo: true });
     if (request.error)
       return interaction.followUp({
         content: 'Введите корректную ссылку на изображение!',
         ephemeral: true,
       });
-    const nekoEmbed: Discord.APIEmbed = {
-      title: 'trace.moe',
+    const tmEmbed: Discord.APIEmbed = {
+      title: 'Поиск аниме по кадру',
       color: Number(config.LINE_COLOR),
       fields: [
-        { name: 'Ближайшее совпадение', value: request.result[0].filename },
         {
-          name: 'Эпизод',
-          value: request.result[0].episode as unknown as string,
+          name: 'Название',
+          value:
+            (request.result[0].anilist.isAdult ? '🔞 ' : '') +
+            (request.result[0].anilist.title.english
+              ? request.result[0].anilist.title.english
+              : request.result[0].anilist.title.romaji
+              ? request.result[0].anilist.title.romaji
+              : request.result[0].anilist.title.native
+              ? request.result[0].anilist.title.native
+              : '~ Не удалось найти название ~'),
         },
         {
           name: 'Сходство',
           value: (request.result[0].similarity * 100).toFixed(2) + ' %',
         },
+        {
+          name: 'Время',
+          value:
+            getTime(request.result[0].from) +
+            '–' +
+            getTime(request.result[0].to),
+        },
       ],
       image: { url: request.result[0].image },
       footer: new Footer(interaction),
     };
+    if (request.result[0].episode) {
+      tmEmbed.fields?.splice(1, 0, {
+        name: 'Эпизод',
+        value: request.result[0].episode as unknown as string,
+      });
+    }
+    const component =
+      new Discord.ActionRowBuilder<Discord.ButtonBuilder>().addComponents(
+        new Discord.ButtonBuilder()
+          .setLabel('Смотреть видеофрагмент')
+          .setStyle(Discord.ButtonStyle.Link)
+          .setURL(request.result[0].video)
+      );
     return await interaction.followUp({
-      embeds: [nekoEmbed],
+      embeds: [tmEmbed],
+      components: [component],
     });
   },
 };
--- src/custom/commonFunctions.ts
+++ src/custom/commonFunctions.ts
@@ -1,20 +1,21 @@
 import Discord from 'discord.js';
 
-const formateDate = (date: Date) => {
-  return (
-    Discord.time(Math.floor(+date / 1000), 'F') +
-    ' (' +
-    Discord.time(Math.floor(+date / 1000), 'R') +
-    ')'
-  );
-  /*return date.toLocaleDateString('ru', {
-    year: 'numeric',
-    month: 'long',
-    day: 'numeric',
-    hour: 'numeric',
-    minute: 'numeric',
-    second: 'numeric',
-  }) as string;*/
+const formateDate = (date: Date) =>
+  Discord.time(Math.floor(+date / 1000), 'F') +
+  ' (' +
+  Discord.time(Math.floor(+date / 1000), 'R') +
+  ')';
+
+const getTime = (input: number) => {
+  const ms = parseInt(input.toString(), 10),
+    hrs = Math.floor(ms / 3600),
+    min = Math.floor((ms - 3600 * hrs) / 60),
+    sec = ms - 3600 * hrs - 60 * min;
+  return [
+    hrs.toString().padStart(2, '0'),
+    min.toString().padStart(2, '0'),
+    sec.toString().padStart(2, '0'),
+  ].join(':');
 };
 
 const getRandomInt = (max: number) =>
@@ -25,4 +26,10 @@ const getRandArrIndex = (arr: Array<any>) =>
 
 const getRandArrElement = (arr: Array<any>) => arr[getRandArrIndex(arr)];
 
-export { formateDate, getRandomInt, getRandArrIndex, getRandArrElement };
+export {
+  formateDate,
+  getTime,
+  getRandomInt,
+  getRandArrIndex,
+  getRandArrElement,
+};
