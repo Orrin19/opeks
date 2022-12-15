db0a27fd02c80294fc5ecc4336423a507a078a9d
Orrin19 <daniil.orrin@yandex.ru>
2022-12-15 21:51:43 +0700

    catching error

--- src/listeners/guildMemberRemove.ts
+++ src/listeners/guildMemberRemove.ts
@@ -7,7 +7,9 @@ export default (client: Discord.Client): void => {
       const channel = member.guild.channels.cache.find(
         (c) => c.name == 'checkuser'
       ) as Discord.TextChannel;
-      channel.send(`${Discord.userMention(member.id)} куда-то ушёл...`);
+      channel
+        .send(`${Discord.userMention(member.id)} куда-то ушёл...`)
+        .catch(console.error);
     }
   );
 };
