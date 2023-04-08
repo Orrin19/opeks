c43a1af9caacc9e67cbd7fa02b10162a5df48701
Orrin19 <daniil.orrin@yandex.ru>
2023-04-08 23:45:45 +0700

    new digger role parser

--- src/Parsers.ts
+++ src/Parsers.ts
@@ -1,5 +1,6 @@
 import { TextParser } from './TextParser';
 import { BotQuestion } from './parsers/BotQuestion';
+import { Digger } from './parsers/Digger';
 import { LamoraunLinks } from './parsers/LamoraunLinks';
 import { Marsel } from './parsers/Marsel';
 import { RandomEmoji } from './parsers/RandomEmoji';
@@ -7,6 +8,7 @@ import { ReactTrigger } from './parsers/ReactTrigger';
 
 export const Parsers: TextParser[] = [
   BotQuestion,
+  Digger,
   LamoraunLinks,
   Marsel,
   RandomEmoji,
