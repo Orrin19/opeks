f92bcf4b5af74683e3f2917b32c279309c6350c8
Orrin19 <ofandomuno@gmail.com>
2024-01-11 21:39:08 +0700

    remove digger

--- src/parsers/Parsers.ts
+++ src/parsers/Parsers.ts
@@ -1,6 +1,5 @@
 import { TextParser } from './TextParser';
 import { BotQuestion } from './storage/BotQuestion';
-import { Digger } from './storage/Digger';
 import { LamoraunLinks } from './storage/LamoraunLinks';
 import { Marsel } from './storage/Marsel';
 import { RandomEmoji } from './storage/RandomEmoji';
@@ -8,7 +7,6 @@ import { ReactTrigger } from './storage/ReactTrigger';
 
 export const Parsers: TextParser[] = [
   BotQuestion,
-  Digger,
   LamoraunLinks,
   Marsel,
   RandomEmoji,
