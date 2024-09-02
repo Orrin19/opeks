86d17c3c28594a5117c09f8165b5b90706d9b8d0
Orrin19 <ofandomuno@gmail.com>
2024-09-02 17:04:49 +0700

    univermag channel parser added

--- src/parsers/Parsers.ts
+++ src/parsers/Parsers.ts
@@ -4,6 +4,7 @@ import { LamoraunLinks } from './storage/LamoraunLinks';
 import { Marsel } from './storage/Marsel';
 import { RandomEmoji } from './storage/RandomEmoji';
 import { ReactTrigger } from './storage/ReactTrigger';
+import { Univermag } from './storage/Univermag';
 
 export const Parsers: TextParser[] = [
   BotQuestion,
@@ -11,4 +12,5 @@ export const Parsers: TextParser[] = [
   Marsel,
   RandomEmoji,
   ReactTrigger,
+  Univermag,
 ];
